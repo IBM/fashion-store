10a17c1e2319e9f8636e63eab2143bf349b86ee4
alvin cho <acho@us.ibm.com>
2018-01-31 03:13:02 -0500

    updated run.sh, fixed main.js psd2 json, add slash for gateway url

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+#README:
+#usage: ./build.sh -p 8080
+
+PORT=8080
+
+while getopts "p:" option; do
+    case ${option}
+        in
+        p) PORT=${OPTARG};;
+    esac
+done
+
 #seems i need to execute the dockerfile at the root to find src
 cp -f ../build/Dockerfile ..
 
 cd ..
-docker build -t shoe-store .
+docker build --no-cache --build-arg "PORT=$PORT" -t shoe-store .
 
 rm -f Dockerfile
 
--- bin/run.sh
+++ bin/run.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-TPPURLENV=
+#README:
+#usage: ./run.sh -e 5000 -c ../configuration/api-endpoint-controller-config.json
+
+EXTERNALPORT=8080
+INTERNALPORT=8080
+PORT=8080
+GATEWAYURL=../configuration/api-endpoint-controller-config.json
+
+while getopts "g:e:i:p:" option; do
+    case ${option}
+        in
+        g) GATEWAYURL=${OPTARG};;
+        e) EXTERNALPORT=${OPTARG};;
+        i) INTERNALPORT=${OPTARG};;
+        p) PORT=${OPTARG};;
+    esac
+done
+
+docker stop shoes
 
 #do the build for you then start
-./build.sh
+./build.sh -p $PORT
 
-if [ "$1" != "" ]; then
-    TPPURLENV="-e \"GATEWAYURL=$1\""
-fi
-
-echo $TPPURLENV
-docker run -d --rm --name shoes -p 8080:8080 -e "GATEWAYURL=$1" shoe-store
+docker run -d --rm --name shoes -p $PORT:$PORT -e "GATEWAYURL=$GATEWAYURL" shoe-store
