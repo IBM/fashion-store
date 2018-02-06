96007e794171167d12e45d2c1d75661160207ce4
Alvin Cho <acho@us.ibm.com>
2018-02-06 08:15:20 -0800

    fixed it so that the redirect takes the URL from the incoming user, not localhost which is then inside the container. startup means we now have a EXTERNALURL to look at

--- bin/run.sh
+++ bin/run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 #README:
-#usage: ./run.sh -e 5000 -c ../configuration/api-endpoint-controller-config.json
+#usage: ./run.sh -e 5000 -g http://athena1.fyre.ibm.com:8400/open-banking -x http://athena1.fyre.ibm.com:9090
 
 EXTERNALPORT=8080
 INTERNALPORT=8080
 PORT=8080
-GATEWAYURL=../configuration/api-endpoint-controller-config.json
+GATEWAYURL="http://athena1.fyre.ibm.com:32756/open-banking"
+EXTERNALURL="http://localhost:8080"
 
-while getopts "g:e:i:p:" option; do
+while getopts "x:g:e:i:p:" option; do
     case ${option}
         in
         g) GATEWAYURL=${OPTARG};;
+        x) EXTERNALURL=${OPTARG};;
         e) EXTERNALPORT=${OPTARG};;
         i) INTERNALPORT=${OPTARG};;
         p) PORT=${OPTARG};;
@@ -21,6 +23,7 @@ done
 docker stop shoes
 
 #do the build for you then start
-./build.sh -p $PORT
+./build.sh -p $EXTERNALPORT
 
-docker run -d --rm --name shoes -p $PORT:$PORT -e "GATEWAYURL=$GATEWAYURL" shoe-store
+#docker run -d --rm --name shoes -p $EXTERNALPORT:8080 -e "GATEWAYURL=$GATEWAYURL" shoe-store
+docker run -d --restart always --name shoes -p $EXTERNALPORT:8080 -e "GATEWAYURL=$GATEWAYURL" -e "EXTERNALURL=$EXTERNALURL" shoe-store
