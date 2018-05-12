8c97dd6be237dd0c7240b1d9d30b0755c7dde031
remkohdev <remkohdev@us.ibm.com>
2018-05-11 21:19:42 -0400

    deploy to icp-k8s - step1

--- k8s-deploy.sh
+++ k8s-deploy.sh
@@ -1,50 +1,38 @@
-echo '----------------------------'
-echo '=====>build shoe-store<====='
-echo '----------------------------'
-docker build --no-cache -t shoe-store .
-echo '--------------------------'
-echo '=====>tag shoe-store<====='
-echo '--------------------------'
-# docker tag shoe-store 192.168.99.100:30500/banksy-ns/shoe-store:latest
-docker tag shoe-store 192.168.99.100:30500/banksy-ns/shoe-store:latest
+echo '-------------------------------'
+echo '=====>build fashion-store<====='
+echo '-------------------------------'
+docker build --no-cache -t fashion-store .
+echo '-----------------------------'
+echo '=====>tag fashion-store<====='
+echo '-----------------------------'
+docker tag fashion-store:latest ip-banksy-repo1-docker-local.artifactory.swg-devops.com/ip-banksy-repo1-docker-local/fashion-store:latest
 echo '-----------------------------'
 echo '=====>login to registry<====='
 echo '-----------------------------'
-#docker login -u admin -p passw0rd 192.168.99.100:30500/banksy-ns
-docker login -u admin -p passw0rd 192.168.99.100:30500/banksy-ns
-echo '---------------------------'
-echo '=====>push shoe-store<====='
-echo '---------------------------'
-#docker push 192.168.99.100:30500/banksy-ns/shoe-store:latest
-docker push 192.168.99.100:30500/banksy-ns/shoe-store:latest
-#echo '------------------------------------'
-#echo '=====>delete shoe-store-deploy<====='
-#echo '------------------------------------'
-#kubectl delete deployment --namespace=banksy-ns shoe-store-deploy
-#sleep 35
-#echo '------------------------------------'
-#echo '=====>create shoe-store-deploy<====='
-#echo '------------------------------------'
-#kubectl create -f ./helm/templates/deployment.json
-echo '---------------------------------'
-echo '=====>delete shoe-store-pod<====='
-echo '---------------------------------'
-kubectl delete pod --namespace=banksy-ns shoe-store-pod
+docker login -u 'username' -p 'password' https://ip-banksy-repo1-docker-local.artifactory.swg-devops.com
+sleep 5
+echo '------------------------------'
+echo '=====>push fashion-store<====='
+echo '------------------------------'
+docker push ip-banksy-repo1-docker-local.artifactory.swg-devops.com/ip-banksy-repo1-docker-local/fashion-store:latest
+echo '------------------------------------'
+echo '=====>delete fashion-store-pod<====='
+echo '------------------------------------'
+kubectl delete pod --namespace=your-ns fashion-store-pod
 sleep 25
-echo '---------------------------------'
-echo '=====>create shoe-store-pod<====='
-echo '---------------------------------'
+echo '------------------------------------'
+echo '=====>create fashion-store-pod<====='
+echo '------------------------------------'
 kubectl create -f ./helm/templates/pod.yaml
-#kubectl create -f <(istioctl kube-inject -f ./helm/templates/pod.yaml)
-echo '---------------------------------'
-echo '=====>delete shoe-store-svc<====='
-echo '---------------------------------'
-kubectl delete svc --namespace=banksy-ns shoe-store-svc
+echo '------------------------------------'
+echo '=====>delete fashion-store-svc<====='
+echo '------------------------------------'
+kubectl delete svc --namespace=your-ns fashion-store-svc
 sleep 1
-echo '---------------------------------'
-echo '=====>create shoe-store-svc<====='
-echo '---------------------------------'
+echo '------------------------------------'
+echo '=====>create fashion-store-svc<====='
+echo '------------------------------------'
 kubectl create -f ./helm/templates/svc.yaml
-echo '-------------------------------'
-echo '=====>shoe-store deployed<====='
-echo '-------------------------------'
+echo '----------------------------------'
+echo '=====>fashion-store deployed<====='
+echo '----------------------------------'
