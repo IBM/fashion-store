2af8477d27571b195d6fe810dba50eab2a18ded1
Sridhar Mooghala <Sridhar.Mooghala@ibm.com>
2018-09-17 22:59:52 -0400

    renamed from nike to citi

--- k8s-deploy-citi.sh
+++ k8s-deploy-citi.sh
@@ -32,7 +32,7 @@ echo '------------------------------------'
 echo '=====>create fashion-store-pod<====='
 echo '------------------------------------'
 
-kubectl create -f ./helm/templates/pod-nike.yaml
+kubectl create -f ./helm/templates/pod-citi.yaml
 
 echo '------------------------------------'
 echo '=====>delete fashion-store-svc<====='
@@ -44,7 +44,7 @@ echo '------------------------------------'
 echo '=====>create fashion-store-svc<====='
 echo '------------------------------------'
 
-kubectl create -f ./helm/templates/svc-nike.yaml
+kubectl create -f ./helm/templates/svc-citi.yaml
 
 echo '----------------------------------'
 echo '=====>fashion-store deployed<====='
